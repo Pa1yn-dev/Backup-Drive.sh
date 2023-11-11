4c09b0fe009e0e70813da52f974f83ce8db7adfb
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 19:58:53 +0000

    Update BackupgDrive.sh

--- BackupgDrive.sh
+++ BackupgDrive.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
+RCLONE_CONFIG=/home/peter/.config/rclone/rclone.conf
+export RCLONE_CONFIG
 
-rclone copy --create-empty-src-dirs /samba/sharedfilesystem/Peter google-drive:Linux-Peter
-rclone copy --create-empty-src-dirs /samba/sharedfilesystem/Andrew google-drive:Linux-Andrew
-rclone copy --create-empty-src-dirs /samba/sharedfilesystem/Steven google-drive:Linux-Steven
-rclone copy --create-empty-src-dirs /samba/sharedfilesystem/Suzanne google-drive:Linux-Suzanne
-rclone copy --create-empty-src-dirs /samba/sharedfilesystem/Server-Information google-drive:Linux-Server-Info
-
+rclone copy --create-empty-src-dirs /samba/sharedfilesystem Drive:SharedFilesystem
